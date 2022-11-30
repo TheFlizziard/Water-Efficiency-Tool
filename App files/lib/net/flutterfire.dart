4c0ffaf2a2e6b8be05bbd302717674cf381ad006
takara <takaraizzahutama@yahoo.com>
2022-11-30 19:03:03 +0900

    added error message

--- App files/lib/net/flutterfire.dart
+++ App files/lib/net/flutterfire.dart
@@ -1,31 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-Future<bool> signIn(String email, String password) async {
+Future signIn(String email, String password) async {
   try {
     await FirebaseAuth.instance
         .signInWithEmailAndPassword(email: email, password: password);
     return true;
-  } catch (e) {
-    print(e);
-    return false;
+  } on FirebaseAuthException catch (e) {
+    Fluttertoast.showToast(msg: e.toString(), gravity: ToastGravity.TOP);
   }
 }
 
-Future<bool> register(String email, String password) async {
+Future register(String email, String password) async {
   try {
     await FirebaseAuth.instance
         .createUserWithEmailAndPassword(email: email, password: password);
     return true;
   } on FirebaseAuthException catch (e) {
-    if (e.code == 'weak-password') {
-      print('The password provided is too weak.');
-    } else if (e.code == 'email-already-in-use') {
-      print('The account already exists for that email.');
-    }
-    return false;
-  } catch (e) {
-    print(e.toString());
-    return false;
+    Fluttertoast.showToast(msg: e.toString(), gravity: ToastGravity.TOP);
   }
 }
